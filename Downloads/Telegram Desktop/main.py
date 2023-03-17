6115db7bff4440c03b5cfab4535d5c3acf4c5c2a
220104vika <vikat63687@gmail.com>
2023-03-17 10:24:10 +0300

    Initial commit

---
+++ Downloads/Telegram Desktop/main.py
@@ -0,0 +1,37 @@
+
+""""import random
+def z1():
+    a = []
+    for i in range(5):
+        a.append(random.randint(-100, 100))
+    b = str(input("Введите число"))
+    if b in list('a'):
+        print (a,b,'Поздравляю, Вы угадали число')
+    else:
+        print(a,b,'нет такого числа')"""""
+
+
+""""def z2():
+    spisok = list('123456')
+    for c in range(len(spisok)):
+        if spisok[c-1] == spisok[c]:
+            print("Значения совпадают")
+    else:
+        print("Значения не совпадают")"""""
+
+""""def z3():
+    dni = ("понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье")
+    kol = int(input('сколько дней выходных - '))
+    for i in dni:
+        print("Выходные", *dni[:-kol])
+        print("Будни", *dni[-kol:])
+        break"""
+
+def z4():
+
+
+
+
+""""z1()"""
+"""z2()"""
+""""z3()"""
