0a1419d8e695cbf754476800395d9b478580cb48
220104vika <vikat63687@gmail.com>
2023-03-17 10:48:20 +0300

    Lab5

--- Downloads/Telegram Desktop/main.py
+++ Downloads/Telegram Desktop/main.py
@@ -1,5 +1,5 @@
 
-""""import random
+import random
 def z1():
     a = []
     for i in range(5):
@@ -8,30 +8,47 @@ def z1():
     if b in list('a'):
         print (a,b,'Поздравляю, Вы угадали число')
     else:
-        print(a,b,'нет такого числа')"""""
+        print(a,b,'нет такого числа')
+import random
 
-
-""""def z2():
+def z2():
     spisok = list('123456')
     for c in range(len(spisok)):
         if spisok[c-1] == spisok[c]:
             print("Значения совпадают")
     else:
-        print("Значения не совпадают")"""""
+        print("Значения не совпадают")
 
-""""def z3():
+def z3():
     dni = ("понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье")
     kol = int(input('сколько дней выходных - '))
     for i in dni:
-        print("Выходные", *dni[:-kol])
-        print("Будни", *dni[-kol:])
-        break"""
+        print("Будни", *dni[:-kol])
+        print("Выходные", *dni[-kol:])
+        break
 
 def z4():
+    spisok_1 = ("Иванов", "Курпатов", "Сизин", "Лортв", "Жаммин", "Эшиль", "Копит", "Тригг", "Моксан", "Привел")
+    spisok_2 = ("Кушнерова", "Кучерук", "Антонова", "Иванов", "Орит", "Каромит", "Шолтим", "Капрон", "Норман", "Хангук")
+    kom = random.randint(1, 10)
+    kom_1 = spisok_1[kom : kom -5]
+    kom_2 = spisok_2[kom : kom +5]
+    kmand = tuple(kom_2 + kom_1)
+    print(kom_1)
+    print(kom_2)
+    print(kmand)
+    print(len(kmand))
+    print(sorted(kmand))
+    if "Иванов" in kmand:
+        print("Иванов есть в команде")
+    else:
+        print("Иванова нет в команде")
 
-
-
-
-""""z1()"""
-"""z2()"""
-""""z3()"""
+print("1 задание")
+z1()
+print("2 задание")
+z2()
+print("3 задание")
+z3()
+print("4 задание")
+z4()
